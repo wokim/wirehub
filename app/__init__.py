0086b5284f750061b29af82d0e4639f263dcf16e
Wonshik KIM <wonshikkim.kr@gmail.com>
2024-03-10 22:08:16 +0900

    refactor: integrate board initialization with Flask app factory

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,8 @@
 from flask import Flask
 from .api import api_bp
+from app.gpio import detect_board, print_board_status, board, init_board, cleanup_board
+import time
+import atexit
 
 def create_app():
     # Create a Flask application instance
@@ -7,4 +10,16 @@ def create_app():
 
     app.register_blueprint(api_bp, url_prefix='/api')
 
+    atexit.register(cleanup_board)
+
     return app
+
+def initialize_board():
+    detect_board()
+    while board.begin() != board.STA_OK:  # Board begin and check board status
+        print_board_status()
+        logger.error("Board begin failed")
+        time.sleep(2)
+    print_board_status()
+    init_board()
+    logger.info("Board begin success")
--- run.py
+++ run.py
@@ -1,30 +1,10 @@
 import logging
 from app import create_app
-from app.gpio import detect_board, print_board_status, board, init_board, cleanup_board
-import atexit
-import time
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 app = create_app()
 
-def initialize_board():
-    detect_board()
-    while board.begin() != board.STA_OK:  # Board begin and check board status
-        print_board_status()
-        logger.error("Board begin failed")
-        time.sleep(2)
-    print_board_status()
-    init_board()
-    logger.info("Board begin success")
-
-atexit.register(cleanup_board)
-
 if __name__ == '__main__':
-    try:
-        initialize_board()
-        app.run(debug=True, port=5000, host='0.0.0.0')
-    finally:
-        # Cleanup GPIO to release resources
-        cleanup_board()
+    app.run(debug=True, port=5000, host='0.0.0.0')
